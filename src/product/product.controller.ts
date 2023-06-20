9207acd5c82dc2876ee43447216fa42dbea56054
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2023-06-20 15:24:03 +0600

    added schema, service, controller and interface

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,7 +1,13 @@
 import { Controller, Body, Post } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { Product } from './product.schema';
 
 @Controller('product')
 export class ProductController {
-    @Post()
-    createProduct(@Body()){}
+  constructor(private readonly productService: ProductService) {}
+
+  @Post()
+  createProduct(@Body() product: Product): Promise<Product> {
+    return this.productService.create(product);
+  }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Product, ProductDocument } from './product.schema';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(
+    @InjectModel(Product.name)
+    private readonly productModel: Model<ProductDocument>,
+  ) {}
+
+  async create(product: Product): Promise<Product> {
+    const createdProduct = new this.productModel(product);
+    return createdProduct.save();
+  }
+}
