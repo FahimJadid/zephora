85bc2afa7bd148fbfd3d07064cfea1ef2220a515
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2023-06-21 14:59:04 +0600

    updated relation between service, schema, controllerand added user-details interface

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './user.schema';
+import { UserDetails } from './user-details.interface';
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectModel(User.name)
+    private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  _getUserDetails(user: UserDocument): UserDetails {
+    const { name, email } = user;
+    return { name, email };
+  }
+}
